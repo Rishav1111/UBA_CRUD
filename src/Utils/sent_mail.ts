00e5dc42401adf573a0abe6960042829546b4a34
RishavSht <np03cs4s220093@heraldcollege.edu.np>
2024-08-13 12:57:31 +0545

    some changes on sent mail

--- src/Utils/sent_mail.ts
+++ src/Utils/sent_mail.ts
@@ -16,7 +16,17 @@ export const sentInvitationEmail = async (email: string, token: string) => {
       from: "IMS <shrestharishav3@gmail.com>",
       to: email,
       subject: "Invitation to register",
-      html: `<h1> You have been invited to register. Click the link below to complete your registration: </h1>\n\nhttp://localhost:5173//register?token=${token}`,
+      html: `<div style="font-family: Arial, sans-serif; padding: 20px;">
+      <h1 style="color: #333;">Welcome to UBA-IMS!</h1>
+      <p style="font-size: 16px; color: #555;">
+        You have been invited to join our platform. To complete your registration, please click the link below:
+      </p>
+      "http://localhost:5173/register/user?token=${token}" 
+         
+      <p style="font-size: 14px; color: #777; margin-top: 20px;">
+        If you did not request this, please ignore this email.
+      </p>
+    </div>`,
     };
     // console.log(transporter);
     const info = await transporter.sendMail(mailOptions);
